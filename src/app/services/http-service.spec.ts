5287a83bebd73c801dc8ed66915e43bc88800da5
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-22 12:40:03 +0200

    feat: add http service provider to handle GET requests

---
+++ src/app/services/http-service.spec.ts
@@ -0,0 +1,93 @@
+import { httpService } from './http-service';
+import * as productItemsFixtures from '@frontend-cart/api/fixtures/product-items';
+
+describe('httpService', () => {
+  let fetchSpy: jest.SpyInstance<Promise<unknown>>;
+  const endpointUrlMock = 'https://test.dev/api/v1/data';
+
+  global.fetch = jest.fn().mockResolvedValue({
+    ok: true,
+    status: 200,
+    json: jest.fn().mockReturnValue(productItemsFixtures)
+  });
+
+  beforeEach(() => {
+    fetchSpy = jest.spyOn(global, 'fetch');
+  });
+
+  afterEach(() => jest.clearAllMocks());
+
+  it('should pass a valid request URL to the fetch middleware', () => {
+    httpService(endpointUrlMock);
+
+    expect(fetchSpy)
+      .toHaveBeenCalledWith('https://test.dev/api/v1/data');
+  });
+
+  it('should return a valid product items recordset if the API responds successfully', async () => {
+    const apiResponse = await httpService(endpointUrlMock);
+
+    expect(apiResponse)
+      .toEqual(productItemsFixtures);
+  });
+
+  it('should statically type the returning recordset if the API responds successfully', async () => {
+    type expectedType = Array<{ id: string; code: string; name: string; price: number }>;
+    const isExpectedType = (input: any): input is expectedType =>
+      (input as expectedType)[0].id !== undefined &&
+      (input as expectedType)[0].code !== undefined &&
+      (input as expectedType)[0].name !== undefined &&
+      (input as expectedType)[0].price !== undefined;
+
+    const apiResponse = await httpService<expectedType>(endpointUrlMock);
+
+    expect(isExpectedType(apiResponse))
+      .toBeTruthy();
+  });
+
+  it('should reject the returning promise if the fetch middleware throws an exception', async () => {
+    fetchSpy.mockRejectedValue(new Error('test error'));
+
+    await expect(httpService(endpointUrlMock)).rejects.toThrow('test error');
+  });
+
+  it('should reject the returning promise if the response is not ok', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: false,
+    });
+
+    await expect(httpService(endpointUrlMock)).rejects.toThrow('Invalid Response');
+  });
+
+  it('should reject the returning promise if the response HTTP status is below 200', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      status: 190,
+      ok: true,
+    });
+
+    await expect(httpService(endpointUrlMock)).rejects.toThrow('Http Error 190');
+  });
+
+  it('should reject the returning promise if the response HTTP status is equals or above 300', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      status: 300,
+      ok: true,
+    });
+
+    await expect(httpService(endpointUrlMock)).rejects.toThrow('Http Error 300');
+
+    global.fetch = jest.fn().mockResolvedValue({
+      status: 404,
+      ok: true,
+    });
+
+    await expect(httpService(endpointUrlMock)).rejects.toThrow('Http Error 404');
+
+    global.fetch = jest.fn().mockResolvedValue({
+      status: 500,
+      ok: true,
+    });
+
+    await expect(httpService(endpointUrlMock)).rejects.toThrow('Http Error 500');
+  });
+});
---
+++ src/app/services/http-service.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+/**
+ * Http Status mapping enum
+ */
+enum HttpStatus {
+  Success     = 200,
+  Redirect    = 300,
+  ClientError = 400,
+  ServerError = 500
+}
+/* eslint-enable @typescript-eslint/no-magic-numbers */
+
+/**
+ * General purpose network client for submitting GET request to a given endpoint URL with extended support for typed responses
+ * @param endpointUrl URL of remote resource
+ * @returns Typed promise with response output, featuring error handling functionality
+ */
+export const httpService = <T>(endpointUrl: string): Promise<T> => {
+  return fetch(endpointUrl)
+    .then((response: Response) => {
+      if (response.ok && response.status >= HttpStatus.Success && response.status < HttpStatus.Redirect) {
+        return response.json();
+      } else {
+        const errorMessage = !response.ok ? 'Invalid Response' : `Http Error ${response.status}`;
+        throw new Error(errorMessage);
+      }
+    })
+    .then((data) => data as T)
+    .catch((error: Error) => Promise.reject(error));
+};
