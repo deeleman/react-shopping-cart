ee480f7452d00372d9894ee7a581cc9f8af812a3
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-22 12:39:34 +0200

    feat: added aliased data fixtures and global alias path

--- jest.config.js
+++ jest.config.js
@@ -79,7 +79,9 @@ module.exports = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    '@frontend-cart/api\/fixtures(.*)$': '<rootDir>/app/api/fixtures/$1'
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
