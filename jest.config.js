df50bdaa6d1c66a11a1b15cf74e13ce8387e2c88
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-22 12:46:03 +0200

    refactor: renamed global alias token

--- jest.config.js
+++ jest.config.js
@@ -80,7 +80,7 @@ module.exports = {
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    '@frontend-cart/api\/fixtures(.*)$': '<rootDir>/app/api/fixtures/$1'
+    '@shopping-cart/api\/fixtures(.*)$': '<rootDir>/app/api/fixtures/$1'
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
