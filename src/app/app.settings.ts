ef60d0f91a6280d6a2721f7f3e8dbc10e45018cc
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-24 02:09:32 +0200

    feat: add configurable options file

---
+++ src/app/app.settings.ts
@@ -0,0 +1,6 @@
+// Main settings - edit endpoints below at your convenience
+const items     = '/api/fixtures/product-items.json';
+const discounts = '/api/fixtures/product-discounts.json';
+
+// Do not edit anything below this line
+export const httpSettings: Record<'items' | 'discounts', string> = { items, discounts };
