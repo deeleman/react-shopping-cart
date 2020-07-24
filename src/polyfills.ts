73a121ddeff28b33e3de444dccb18c3e56d195bc
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-24 02:09:53 +0200

    refactor: add type to polyfill function signature

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -5,7 +5,7 @@ import 'whatwg-fetch';
 // https://tc39.github.io/ecma262/#sec-array.prototype.find
 if (!Array.prototype.find) {
   Object.defineProperty(Array.prototype, 'find', {
-    value: function(predicate) {
+    value: function(predicate: unknown) {
      // 1. Let O be ? ToObject(this value).
       if (this == null) {
         throw new TypeError('"this" is null or not defined');
