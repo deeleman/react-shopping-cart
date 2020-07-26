7ba76805c672d13f48c6b640e2751d9790d70319
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-26 02:48:39 +0200

    feature: add centralized settings file

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,6 +1,8 @@
+import { PricingRules } from './types';
+
 // Main settings - edit endpoints below at your convenience
-const items     = '/api/fixtures/product-items.json';
-const discounts = '/api/fixtures/product-discounts.json';
+const items         = '/api/fixtures/product-items.json';
+const discountRules = '/api/fixtures/product-discounts.json';
 
 // Do not edit anything below this line
-export const httpSettings: Record<'items' | 'discounts', string> = { items, discounts };
+export const httpSettings: Record<keyof PricingRules, string> = { items, discountRules };
