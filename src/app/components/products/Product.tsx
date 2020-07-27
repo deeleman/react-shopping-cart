29003acf43bd9c100bd62bc38ddbdc86858ed183
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-27 21:52:57 +0200

    feat: add fallback image for products with no matching image

--- src/app/components/products/Product.tsx
+++ src/app/components/products/Product.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import images from './../../../img/*.png';
+import { images } from './../../../img';
 
 interface ProductProps {
   shortName: string;
   code: string;
 }
 
-export const Product: React.FunctionComponent<ProductProps> = ({ shortName, code }) => (
-  <figure className="product-image">
-    <img src={images[shortName.toLowerCase()]} alt={shortName} />
-    <div className="product-description">
-      <h1>{shortName}</h1>
-      <p className="product-code">Product code {code}</p>
-    </div>
-  </figure>
-);
+export const Product: React.FunctionComponent<ProductProps> = ({ shortName, code }) => {
+  const image = images[shortName.toLowerCase()] || images['fallback'];
+
+  return (
+    <figure className="product-image">
+      <img role="img" src={image} alt={shortName} />
+      <div className="product-description">
+        <h1 aria-label="product-name">{shortName}</h1>
+        <p aria-label="product-code" className="product-code">Product code {code}</p>
+      </div>
+    </figure>
+  );
+};
---
+++ src/img/index.ts
@@ -0,0 +1,1 @@
+export * as images from './*.png';
