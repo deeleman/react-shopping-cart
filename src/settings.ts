121965bdd72285b4a6ffe778cfc99d5842a3c154
Pablo Deeleman <pablo.deeleman@dynatrace.com>
2020-07-28 20:58:08 +0200

    chore: ensure no port conflicts in hardcoded settings

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,9 @@
 import { PricingSettings } from './app';
 
-// Main settings - edit endpoints below at your convenience to point to an REST API of tyour choice
-const items         = 'http://localhost:9000/api/fixtures/product-items.json';
-const discountRules = 'http://localhost:9000/api/fixtures/product-discounts.json';
+// Main settings - edit endpoints below at your convenience 
+//                 to point to an REST API of your choice:
+const items         = '/api/fixtures/product-items.json';
+const discountRules = '/api/fixtures/product-discounts.json';
 
 // Do not edit anything below this line
 export const settings: PricingSettings = { items, discountRules };
